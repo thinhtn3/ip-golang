89261121c52444d4c4c802afe603e89806d2dfd6
thinhtn3 <thinhttnguyen@gmail.com>
2026-02-07 16:04:13 -0800

    refactor: introduced helpers for chat handlers. Refactored chat handlers with new helpers

--- go-api/internal/handlers/chat/handler.go
+++ go-api/internal/handlers/chat/handler.go
@@ -3,51 +3,41 @@ package chat
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
-	"github.com/supabase-community/gotrue-go/types"
-
 	chatDomain "github.com/thinhtn3/ip-golang.git/internal/domain/chat"
 	chatService "github.com/thinhtn3/ip-golang.git/internal/services/chat"
+	"net/http"
 )
-
 
 type ChatSessionHandler struct {
 	chatService *chatService.ChatService
 }
 
-//constructor for ChatSessionHandler
+
+// constructor for ChatSessionHandler
 func NewChatSessionHandler(chatService *chatService.ChatService) *ChatSessionHandler {
 	return &ChatSessionHandler{chatService: chatService}
 }
 
 
-// HANDLER FUNCTION TO CREATE CHAT SESSION //
-type ChatSessionRequest struct {
-	QuestionID string `json:"question_id"`
-}
-
+// HANDLER FUNCTION TO CREATE CHAT SESSION 
 func (h *ChatSessionHandler) CreateSessionFromQuestion(c *gin.Context) {
-	rawUser, exists := c.Get("user")
-	if !exists {
-		c.JSON(401, gin.H{"message": "Unauthorized"})
-		return
-	}
-	// type assertion from supabase goauth types library
-	user, ok := rawUser.(*types.UserResponse)
-	if !ok {
-		c.JSON(500, gin.H{"message": "Internal server error"})
-		return
-	}
-
-	//bind question id to request
-	req := ChatSessionRequest{}
-	err := c.ShouldBindJSON(&req)
+	// get user id from context
+	userID, err := getUserIDFromContext(c)
 	if err != nil {
-		c.JSON(400, gin.H{"message": "Invalid request"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request"})
 		return
 	}
+	
+	type ChatSessionRequest struct {
+		QuestionID string `json:"question_id"`
+	}
 
-	//create chat session
-	session, err := h.chatService.CreateSession(c.Request.Context(), user.User.ID, uuid.MustParse(req.QuestionID))
+	// bind request to go type
+	req := ChatSessionRequest{}
+	err = bindRequest(c, &req)
+
+	// call service
+	session, err := h.chatService.CreateSession(c.Request.Context(), userID, uuid.MustParse(req.QuestionID))
 	if err != nil {
 		c.JSON(500, gin.H{"message": "Internal server error"})
 		return
@@ -56,82 +46,71 @@ func (h *ChatSessionHandler) CreateSessionFromQuestion(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "Chat session created successfully", "session": session})
 }
 
-// SENDING MESSAGES //
-type MessageRequest struct {
-	Message       string `json:"message"`
-	Role          string `json:"role"`
-}
+
+// SENDING MESSAGES 
 func (h *ChatSessionHandler) SendMessage(c *gin.Context) {
-	//Get user from context
-	rawUser, exists := c.Get("user")
-	if !exists {
-		c.JSON(401, gin.H{"message": "Unauthorized"})
-		return
-	}
-	user, ok := rawUser.(*types.UserResponse)
-	if !ok {
-		c.JSON(500, gin.H{"message": "Internal server error"})
-		return
-	}
-
-	//Parse sessionID string from URL
-	sessionIDStr := c.Param("sessionId")
-	sessionID, err := uuid.Parse(sessionIDStr)
-	if err != nil {
-		c.JSON(400, gin.H{"message": "Invalid session ID format"})
-		return
-	}
-
-	//bind messages into request, which can be accessed as req.Message and req.Role
-	req := MessageRequest{}
-	err = c.ShouldBindJSON(&req)
+	userID, err := getUserIDFromContext(c)
 	if err != nil {
 		c.JSON(400, gin.H{"message": "Invalid request"})
 		return
 	}
+	
 
-	chat, err := h.chatService.SendMessage(c, user.User.ID, sessionID, req.Message, req.Role);
-	if err != nil {
-		if err == chatDomain.ErrForbidden {
-			c.JSON(403, gin.H{"message": "Forbidden: User does not own session"})
-			return
-		} else {
-			c.JSON(500, gin.H{"message": "Internal server error"})
-		}
-		return
-	}
-	c.JSON(200, gin.H{"message": "Succesfully sent", "chat": chat})
-}
-
-// GET MESSAGES INITIAL LOAD//
-func (h *ChatSessionHandler) GetMessages(c *gin.Context) {
-	rawUser, exists := c.Get("user")
-	if !exists {
-		c.JSON(401, gin.H{"message": "Unauthorized"})
-		return
-	}
-	user, ok := rawUser.(*types.UserResponse)
-	if !ok {
-		c.JSON(500, gin.H{"message": "Internal server error"})
-		return
-	}
-
-	sessionIDStr := c.Param("sessionId")
-	sessionID, err := uuid.Parse(sessionIDStr)
+	sessionID, err := parseSessionIDFromContext(c)
 	if err != nil {
 		c.JSON(400, gin.H{"message": "Invalid session ID format"})
 		return
 	}
 
-	messages, err := h.chatService.GetMessages(c.Request.Context(), user.User.ID, sessionID, 0)
+	type MessageRequest struct {
+		Message       string `json:"message"`
+		Role          string `json:"role"`
+	}
+
+	req := MessageRequest{}
+	err = bindRequest(c, &req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request"})
+		return
+	}
+
+	chat, err := h.chatService.SendMessage(c, userID, sessionID, req.Message, req.Role);
 	if err != nil {
 		if err == chatDomain.ErrForbidden {
-			c.JSON(403, gin.H{"message": "Forbidden: User does not own session"})
+			c.JSON(http.StatusForbidden, gin.H{"message": "Forbidden: User does not own session"})
 			return
 		} else {
-			c.JSON(500, gin.H{"message": "Internal server error"})
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
+		}
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Succesfully sent", "chat": chat})
+}
+
+
+// GET MESSAGES INITIAL LOAD
+func (h *ChatSessionHandler) GetMessages(c *gin.Context) {
+	userID, err := getUserIDFromContext(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid request"})
+		return
+	}
+
+	sessionID, err := parseSessionIDFromContext(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid session ID format"})
+		return
+	}
+
+	messages, err := h.chatService.GetMessages(c.Request.Context(), userID, sessionID, 0)
+	if err != nil {
+		if err == chatDomain.ErrForbidden {
+			c.JSON(http.StatusForbidden, gin.H{"message": "Forbidden: User does not own session"})
+			return
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal server error"})
 			return
 		}
 	}
-	c.JSON(200, gin.H{"message": "Successfully retrieved messages", "messages": messages})
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully retrieved messages", "messages": messages})
 }
